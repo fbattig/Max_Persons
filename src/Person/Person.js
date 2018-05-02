e450dff17782b9a1c7736ff59feb63e9c41ade97
Felix <fbattig@yahoo.com>
2018-05-02 05:02:02 -0700

    external Styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
 
-    <div onClick={props.clicked} >
+    <div 
+    className='Person'
+    onClick={props.clicked} >
       <p>I'm {props.name} and I am {props.age} years old</p>
       <p>{props.children} </p>
       <input type='text' onChange={props.changed} value={props.name}/>
