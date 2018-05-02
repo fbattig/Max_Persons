f82d3b1c30e621ea6cfbaf98ee827a9dd13fd8f3
Felix <fbattig@yahoo.com>
2018-05-02 03:59:55 -0700

    implementing SetState

--- src/App.js
+++ src/App.js
@@ -9,12 +9,24 @@ class App extends Component {
        {name: 'Sunamelia' , age: 64},
        {name: 'Urso', age: 66}
     ]
-
   }
+
+    switchNameHandler=()=>{
+     this.setState({
+      persons: [
+        { name: 'Felix Battig', age: 67 },
+        { name: 'Sunamelia', age: 64 },
+        { name: 'Urso Weber', age: 65 }
+      ]
+     }
+     )
+    }
+  
   render() {
     return (
       <div>
        <h1> Hola</h1>
+       <button onClick={this.switchNameHandler} > Switch Name </button>
        <Person  name={this.state.persons[0].name} age={this.state.persons[0].age} />
        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >I like cooking</Person>
        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
